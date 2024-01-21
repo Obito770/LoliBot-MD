72ec46615bc41e6ce05ad910779cefb2feb5c0de
Gcfjjkj <144083670+Gcfjjkj@users.noreply.github.com>
2024-01-21 07:32:50 +0200

    Update buscador-google.js

--- plugins/buscador-google.js
+++ plugins/buscador-google.js
@@ -5,11 +5,15 @@ let handler = async (m, { conn, command, args, usedPrefix }) => {
 const fetch = (await import('node-fetch')).default;
 const text = args.join` `;
 let fkontak = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net" }
-if (!text) return conn.reply(m.chat,  `⚠️ 𝙌𝙪𝙚 𝙚𝙨𝙩𝙖 𝙗𝙪𝙨𝙘𝙖𝙣𝙙𝙤? 🤔 𝙀𝙨𝙘𝙧𝙞𝙗𝙖 𝙡𝙤 𝙦𝙪𝙚 𝙦𝙪𝙞𝙚𝙧𝙖 𝙗𝙪𝙨𝙘𝙖𝙧 𝙀𝙟𝙚𝙢𝙥𝙡𝙤 :\n${usedPrefix + command} loli`, m, {contextInfo: { externalAdReply :{ mediaUrl: null, mediaType: 1, description: null, title: mg, body: '𝐒𝐮𝐩𝐞𝐫 𝐁𝐨𝐭 𝐃𝐞 𝐖𝐡𝐚𝐭𝐬𝐀𝐩𝐩', previewType: 0, thumbnail: imagen4, sourceUrl: [md, yt, tiktok].getRandom()}}})
+if (!text) return conn.reply(m.chat,  `*[❗] ما هو الذي توريد البحث؟ 🤔*
+
+*—◉ امـثـلـه*
+*◉ .بحث سعر الدولار في مصر اليوم*
+*◉ .بحث ما هي منصه هيروكو*\n${usedPrefix + command} `, m, {contextInfo: { externalAdReply :{ mediaUrl: null, mediaType: 1, description: null, title: mg, body: '𝐒𝐮𝐩𝐞𝐫 𝐁𝐨𝐭 𝐃𝐞 𝐖𝐡𝐚𝐭𝐬𝐀𝐩𝐩', previewType: 0, thumbnail: imagen4, sourceUrl: [md, yt, tiktok].getRandom()}}})
 try {
 const url = 'https://google.com/search?q=' + encodeURIComponent(text);
 google({'query': text}).then(res => {
-let teks = `*🔍 RESULTADOS DE:* ${text}\n\n*${url}*\n\n`
+let teks = `*🔍 النـتــائج دي:* ${text}\n\n*${url}*\n\n`
 for (let g of res) {
 teks += `_${g.title}_\n_${g.link}_\n_${g.snippet}_\n\n┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈\n\n`
 } 
@@ -20,9 +24,9 @@ conn.sendFile(m.chat, ss, 'error.png', teks, fkontak)
 } catch {    
 handler.limit = 0
 }}
-handler.help = ['google', 'googlef'].map(v => v + ' <pencarian>')
+handler.help = ['جوجل', 'google'].map(v => v + ' <pencarian>')
 handler.tags = ['internet']
-handler.command = /^googlef?$/i
+handler.command = /^جوجل|google?$/i
 handler.exp = 40
 handler.exp = 3
 handler.limit = 1
